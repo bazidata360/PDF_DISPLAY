021929b76364daf0c264d6f88f9e875e494a4c66
Muhammad Abaz Bashir <138451048+bazidata360@users.noreply.github.com>
2023-10-20 00:37:43 +0500

    Update final.py

--- final.py
+++ final.py
@@ -66,7 +66,7 @@ if st.session_state.login_status:
         col1, col2,col3= st.columns(3)
         with col1:
             if st.button('Read PDF Tutorial',key='1'):
-                show_pdf('cv2.pdf');
+                show_pdf('https://www.annualreports.com/HostedData/AnnualReports/PDF/LSE_BARC_2022.pdf');
         with col2:
             st.button('Close PDF Tutorial',key='2')                   
         with col3:
