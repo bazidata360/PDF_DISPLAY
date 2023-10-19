6a912b6b3f13e09c8b09d3ccd1fc5dbcbf31bd03
Muhammad Abaz Bashir <138451048+bazidata360@users.noreply.github.com>
2023-10-20 02:00:52 +0500

    Update final.py

--- final.py
+++ final.py
@@ -33,7 +33,7 @@ if st.session_state.login_status:
             st.markdown(f"[{user_link}]({user_link})")
     # Embed the PDF viewer using an iframe
             st.write("PDF Viewer:")
-            st.write(f'<embed src="{user_link}" width="400%" height="800"></embed>', unsafe_allow_html=True)
+            st.markdown(f'<embed src="{user_link}" width="400%" height="800"></embed>', unsafe_allow_html=True)
 
     def display_NYSE_RBS_2020():
         st.title("NYSE_RBS_2020")
