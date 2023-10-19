b26128a8b9cacdb57619a6438f70c31d9f91f7dc
Muhammad Abaz Bashir <138451048+bazidata360@users.noreply.github.com>
2023-10-20 00:43:08 +0500

    Update final.py

--- final.py
+++ final.py
@@ -17,7 +17,7 @@ if st.session_state.login_status:
     def show_pdf(file_path):
         with open(file_path,"rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="300" height="800" type="application/pdf"></embed>'
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="300" height="800" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
     
     def display_NYSE_HSBC_2021():
